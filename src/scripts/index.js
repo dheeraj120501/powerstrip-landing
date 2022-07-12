67673d29ef546422285a3938c8a4cead0c2d9c8e
dheeraj120501 <dheerajclan@gmail.com>
2022-07-12 15:59:44 +0530

    new branch created for new design of landing page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,46 @@
 const features = document.querySelectorAll(".feature-box");
-window.addEventListener("scroll", (e) => {
+const main_btn = document.querySelector(".btn-filled");
+const benifit_section = document.querySelector(".benifits");
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".form");
+const navbar = document.querySelector("nav");
+
+main_btn.addEventListener("click", () => {
+  modal.classList.toggle("off");
+});
+
+modal.addEventListener("click", () => {
+  modal.classList.toggle("off");
+});
+
+form.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener("scroll", () => {
+  const benifit_section_rect = benifit_section.getBoundingClientRect();
+  if (benifit_section_rect.top < 0) {
+    main_btn.classList.add("main-btn");
+    navbar.classList.add("nav-fixed");
+  } else {
+    main_btn.classList.remove("main-btn");
+    navbar.classList.remove("nav-fixed");
+  }
   features.forEach((feature) => {
     const content = feature.children[0];
     if (!content.length) {
       const featureRect = feature.getBoundingClientRect();
       const contentRect = content.getBoundingClientRect();
       content.style.opacity =
-        (window.innerHeight / 2 -
-          contentRect.height / 2 -
-          Math.abs(featureRect.top)) /
-        (window.innerHeight / 2 - contentRect.height / 2);
+        featureRect.top < 0
+          ? (window.innerHeight / 2 -
+              contentRect.height / 2 -
+              Math.abs(featureRect.top)) /
+            (window.innerHeight / 2 - contentRect.height / 2)
+          : (window.innerHeight / 2 +
+              contentRect.height / 2 -
+              Math.abs(featureRect.top)) /
+            (window.innerHeight / 2);
     }
   });
 });
