e4f63170db807d94b9e61d49e0c4790f321501f1
dheeraj120501 <dheerajclan@gmail.com>
2022-07-20 20:02:42 +0530

    resolved browser caching issue on production

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
+    filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/[name][ext]",
   },
 
